1f8adb7f8cbdc2061dafb3270df55d3dfeb869e5
Norbyte <infernorb@gmail.com>
2023-08-17 21:11:29 +0200

    Improve console readability

--- BG3Extender/Extender/Shared/Console.cpp
+++ BG3Extender/Extender/Shared/Console.cpp
@@ -322,6 +322,22 @@ void DebugConsole::Create()
 	// Disable Ctrl+C handling
 	SetConsoleCtrlHandler(NULL, TRUE);
 
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	GetCurrentConsoleFontEx(hStdout, FALSE, &cfi);
+
+	// Change to a more readable font if user has one of the default eyesore fonts
+	if (wcscmp(cfi.FaceName, L"Terminal") == 0 || wcscmp(cfi.FaceName, L"Courier New") || (cfi.FontFamily & TMPF_VECTOR) == 0) {
+		cfi.cbSize = sizeof(cfi);
+		cfi.nFont = 0;
+		cfi.dwFontSize.X = 0;
+		cfi.dwFontSize.Y = 14;
+		cfi.FontFamily = FF_MODERN | TMPF_VECTOR | TMPF_TRUETYPE;
+		cfi.FontWeight = FW_NORMAL;
+		wcscpy_s(cfi.FaceName, L"Lucida Console");
+		SetCurrentConsoleFontEx(hStdout, FALSE, &cfi);
+	}
+
 	FILE * outputStream;
 	freopen_s(&outputStream, "CONOUT$", "w", stdout);
 	FILE* inputStream;
