0682ca1f1b19ce212678f52d62c4fdaea4f71c31
Norbyte <infernorb@gmail.com>
2024-09-13 23:43:15 +0200

    Targeting v21

--- BG3Extender/resource.h
+++ BG3Extender/resource.h
@@ -2,8 +2,8 @@
 // Microsoft Visual C++ generated include file.
 // Used by BG3Extender.rc
 //
-#define RES_DLL_MAJOR_VERSION           20
-#define RES_DLL_VERSION_STRING          "20.0.0.0"
+#define RES_DLL_MAJOR_VERSION           21
+#define RES_DLL_VERSION_STRING          "21.0.0.0"
 
 #define IDR_LUA_BUILTIN_BUNDLE          101
 #define IDR_BINARY_MAPPINGS             107
