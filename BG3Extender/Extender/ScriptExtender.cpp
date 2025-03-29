b4f25e57a130ecc54aa4fce050bf436778054032
Norbyte <infernorb@gmail.com>
2025-03-29 20:14:01 +0100

    Bump version requirement

--- BG3Extender/Extender/ScriptExtender.cpp
+++ BG3Extender/Extender/ScriptExtender.cpp
@@ -74,7 +74,7 @@ void ScriptExtender::Initialize()
         } else {
             ERR("Game version v%d.%d.%d.%d is not supported, please upgrade!", gameVersion.Major, gameVersion.Minor, gameVersion.Revision, gameVersion.Build);
             // Hard exit below a certain version as the EoCClient error display UI won't work anymore
-            Fail("Script Extender doesn't support game versions below v4.57, please upgrade!");
+            Fail("Script Extender doesn't support game versions below v4.65, please upgrade!");
         }
     } else {
         ERR("Failed to retrieve game version info.");
