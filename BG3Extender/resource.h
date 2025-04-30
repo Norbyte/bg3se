ebb3a90def6839509b977e60b80dd62825f2970d
Norbyte <infernorb@gmail.com>
2025-04-30 19:51:00 +0200

    Next feature branch is v25

--- BG3Extender/resource.h
+++ BG3Extender/resource.h
@@ -2,8 +2,8 @@
 // Microsoft Visual C++ generated include file.
 // Used by BG3Extender.rc
 //
-#define RES_DLL_MAJOR_VERSION           24
-#define RES_DLL_VERSION_STRING          "24.0.0.0"
+#define RES_DLL_MAJOR_VERSION           25
+#define RES_DLL_VERSION_STRING          "25.0.0.0"
 
 #define IDR_LUA_BUILTIN_BUNDLE          101
 #define IDR_BINARY_MAPPINGS             107
