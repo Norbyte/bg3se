2e88fc85628777e1bf20e1ea539f5957d9693199
Norbyte <infernorb@gmail.com>
2025-06-04 21:53:49 +0200

    Relax variant check requirements

--- BG3Extender/Lua/Helpers/LuaTypeCheckObject.h
+++ BG3Extender/Lua/Helpers/LuaTypeCheckObject.h
@@ -12,7 +12,8 @@ typename bool do_typecheck(lua_State* L, int index, Overload<std::variant<Args..
 template <class T>
 typename std::enable_if_t<!IsByVal<T> && !std::is_pointer_v<T>, bool> do_typecheck(lua_State* L, int index, Overload<T>)
 {
-    return lua_typecheck_cppvalue(L, index, MetatableTag::ObjectRef, StructID<T>::ID);
+    return lua_typecheck_cppvalue(L, index, MetatableTag::ObjectRef, StructID<T>::ID)
+        || lua_type(L, index) == LUA_TTABLE;
 }
 
 template <class T>
@@ -22,7 +23,9 @@ typename std::enable_if_t<!IsByVal<T>&& std::is_pointer_v<T>, bool> do_typecheck
     if constexpr (IsArrayLike<TVal>::Value || IsSetLike<TVal>::Value || IsMapLike<TVal>::Value) {
         return false;
     } else {
-        return lua_typecheck_cppvalue(L, index, MetatableTag::ObjectRef, StructID<TVal>::ID);
+        return lua_typecheck_cppvalue(L, index, MetatableTag::ObjectRef, StructID<TVal>::ID)
+            // Assume any table is compatible with the definition until we figure out a better way
+            || lua_type(L, index) == LUA_TTABLE;
     }
 }
 
